9e339b1ba81461bffb5da7c958b6eaaf38266f5e
Kasianchyk <yka@ciklum.com>
2017-10-12 11:06:23 +0300

    test commit

--- src/main/java/com/naturalint/repository/UserRepository.java
+++ src/main/java/com/naturalint/repository/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Created by skn on 10/11/2017.
  */
 public interface UserRepository  extends JpaRepository<User, String> {
-    public User findByEmail(String email) ;
+    User findByEmail(String email) ;
 }
